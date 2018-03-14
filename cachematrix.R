1ac449e7c88366bb67def9b919c4b6cf4c8f3c87
Tanvi Ranade <phoenix.tanvir@gmail.com>
2018-03-14 18:57:29 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,41 @@
 
 ## Write a short comment describing this function
 
+##Similar to shown in instructions of assignment , this function has 4 functions.
+
 makeCacheMatrix <- function(x = matrix()) {
+  invmat <- NULL
+  set <- function(y){
+    x<<-y
+    invmat<<- NULL
+  }
+  get <- function() x
+  setinv <- function(im) invmat <<- im
+  getinv <- function() invmat
+  list(set = set , get = get , setinv = setinv , getinv = getinv)
 
 }
 
 
 ## Write a short comment describing this function
+##This function uses matplotlib for finding inverse of matrix.
+##argument to cacheSolve is of type makeCacheMatrix
+##The function checks for cached inverse , if present , returns it else calculates and stores 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  library(matlib)
+  m <- x$getinv()
+  if(!is.null(m)){
+    message("getting cached data")
+    return(m)
+  }
+  else
+  {
+    data<-x$get()
+    x$setinv(inv(data))
+    x$getinv()
+    
+  }
 }
+
